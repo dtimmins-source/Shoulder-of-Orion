02a0bb961a37a21448d5bbe93ac6d1a06bc6549d
Dan Timmins <dantimmins97@gmail.com>
2024-04-18 18:33:25 -0400

    Input System added, and basic movement hooked up for the player.

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Vector2 rawInput;
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+}
