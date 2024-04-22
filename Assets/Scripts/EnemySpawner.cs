959cc1266dbaff98b37ee80d9a864344d34c4b2a
Dan Timmins <dantimmins97@gmail.com>
2024-04-22 16:35:10 -0400

    Infinite waves spawning set up with a Do While loop and isLooping bool.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
     WaveConfigSO currentWave; 
 
     void Start()
@@ -22,19 +23,23 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemyWaves()
     {
-        foreach(WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach(WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i), 
-                    currentWave.GetStartingWaypoint().position,
-                    Quaternion.identity,
-                    transform);
-                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i), 
+                        currentWave.GetStartingWaypoint().position,
+                        Quaternion.identity,
+                        transform);
+                        yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
         }
+        while (isLooping);
 
     }
 }
